41d1707837b2e04aa4ae3f363aed8b75f1436a3f
Jedrzej Kostyk <jedrek2504@gmail.com>
2024-03-24 16:08:35 +0100

    configurated transcript, added necessary packages, configurated pm2, added missing scripts

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
 /* Entry point for the application */
+
+import express from 'express';
+
+const app = express();
+
+
+export default app; // Export the app object as the default export
---
+++ start.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+SCRIPT_NICKNAME="Start script"
+VOLUME_GUARD_PATH="./helpers/persistent_volume_guard.sh"
+
+echo "$SCRIPT_NICKNAME: Running packages upgrade"
+apt update && apt upgrade
+
+if [ ! $? -eq 0 ]; then
+    echo "$SCRIPT_NICKNAME: WARNING - Failed to update/upgrade the packages. Proceeding with the current version."
+fi
+
+echo "$SCRIPT_NICKNAME: Setting environment variables..."
+chmod u+rx ./helpers/config.sh
+. ./helpers/config.sh
+
+if [ $? -eq 0 ]; then
+    /bin/sh $VOLUME_GUARD_PATH
+    if [ $? -eq 0 ]; then
+        echo "$SCRIPT_NICKNAME: Setting up database sync..."
+        npm run migrate
+
+        if [ $? -eq 0 ]; then
+            echo "$SCRIPT_NICKNAME: Launching core application..."
+            npm run pm2
+        else
+            echo "$SCRIPT_NICKNAME: ERROR - Failed to setup database sync"
+        fi
+    else
+        echo "$SCRIPT_NICKNAME: ERROR - Persistent volume guard error" 
+    fi
+else 
+    echo "$SCRIPT_NICKNAME: ERROR - Failed to set environment variables, check the config.sh file"
+fi
