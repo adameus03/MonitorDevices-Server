51541f1fcdf1bb681b5825bbc5e13ed810a8d8a1
adamski234 <adamski234@protonmail.com>
2024-05-08 22:54:48 +0200

    Implemented untested communication init

--- src/device_bridge/app.ts
+++ src/device_bridge/app.ts
@@ -1,37 +1,111 @@
 import * as net from "net";
 
-import { OperationType, PacketControlSegment, PacketFromDevice, RawRegistrationPacket } from "./messages";
+import { OperationType, PacketControlSegment, PacketFromDevice, RawInitiateConnectionPacket, RawRegistrationPacket } from "./messages";
 import { registerDevice } from "./registration";
+import { areUint8ArraysEqual } from "./utils";
+import DeviceManager from "../modules/device_management/model/DeviceManager";
+import { randomFillSync } from "crypto";
 
 export class DeviceConnectingServer {
 	private serverInstance;
-	/**
-	 *
-	 */
+	private connectedDevices: ConnectedDeviceInfo[] = [];
+
 	constructor() {
 		this.serverInstance = new net.Server();
 		this.serverInstance.on("connection", (socket) => {
-			socket.on("data", (data) => {
+			socket.on("data", async (data) => {
 				const dataTyped = new Uint8Array(data);
-				console.log(`Received ${dataTyped.length} bytes`);
 				const packet = PacketFromDevice.decodePacket(dataTyped);
-				console.log(`Decoded packet ${packet}`);
-				packet.controlSegment.isResponse = true;
 				switch (packet.controlSegment.operationType) {
+					case OperationType.InitiateConnection: {
+						if (await this.canCreateNewConnection(packet)) {
+							socket.removeAllListeners(); // unbind old listeners - no need to init connections or register
+							this.createNewConnection(socket, packet);
+							console.log("Connected new device");
+						}
+						break;
+					}
 					case OperationType.RegisterDevice: {
 						registerDevice(packet, socket);
 						break;
 					}
 					default: {
-						console.log("Unknown packet in server handler");
+						console.log("Device tried to perform operations despite not being connected");
+						socket.end();
 					}
 				}
 			})
 		});
 	}
 
+	/*
+	 * Communication session init flow:
+	 * Device sends a APP_CONTROL_OP_INITCOMM with application_initcomm_section_t, session ID in control header is empty
+	 * Server decodes that to a pair: OperationType::InitiateConnection and RawInitiateConnectionPacket
+	 * Server verifies that the device is allowed to connect: is registered, the auth key is correct and isn't already connected
+	 * Server generates session ID and fills that into the control header
+	 * Server serializes and sends APP_CONTROL_OP_INITCOMM with application_initcomm_section_t to client
+	 * Server remembers the session and authenticates further operations
+	 */
+	async canCreateNewConnection(packet: PacketFromDevice): Promise<boolean> {
+		const infoPacket = packet.messageContent as RawInitiateConnectionPacket; // needs to be called from a InitiateCommunication packet
+		const deviceDatabase = await DeviceManager.GetDeviceByID(infoPacket.cameraID);
+
+		if (!deviceDatabase) return false; // check if device ID exists
+
+		if (!areUint8ArraysEqual(deviceDatabase.get("auth_key") as Uint8Array, infoPacket.cameraAuthKey)) return false; // check if auth key is correct
+
+		if (this.connectedDevices.find(val => areUint8ArraysEqual(val.deviceID, infoPacket.cameraID))) return false; // check if device already connected
+
+		return true;
+	}
+
+	async createNewConnection(socket: net.Socket, packet: PacketFromDevice) {
+		const infoPacket = packet.messageContent as RawInitiateConnectionPacket; // needs to be called from a InitiateCommunication packet
+		const sessionID = new Uint8Array(16);
+
+		do {
+			randomFillSync(sessionID);
+		} while (this.connectedDevices.find(val => areUint8ArraysEqual(val.sessionID, sessionID)));
+
+		packet.controlSegment.sessionID = sessionID;
+		packet.controlSegment.isResponse = true;
+
+		const deviceInfo = new ConnectedDeviceInfo(socket, infoPacket.cameraID, sessionID);
+		this.connectedDevices.push(deviceInfo);
+
+		socket.write(packet.serialize());
+
+		socket.on("data", data => {
+			const dataTyped = new Uint8Array(data);
+			const packet = PacketFromDevice.decodePacket(dataTyped);
+			switch (packet.controlSegment.operationType) {
+				case OperationType.NoOperation: {
+					deviceInfo.lastPacketReceived = Date.now();
+				}
+				default: {
+					console.log("Unknown packet type");
+					break;
+				}
+			}
+		})
+	}
+
 	start(port: number) {
 		this.serverInstance.listen(port);
+	}
+}
+
+class ConnectedDeviceInfo {
+	socket: net.Socket;
+	deviceID: Uint8Array = new Uint8Array(16);
+	sessionID: Uint8Array = new Uint8Array(16);
+	lastPacketReceived: EpochTimeStamp = Date.now();
+
+	constructor(socket: net.Socket, deviceID: Uint8Array, sessionID: Uint8Array) {
+		this.socket = socket;
+		this.deviceID = deviceID;
+		this.sessionID = sessionID;
 	}
 }
 
--- src/device_bridge/registration.ts
+++ src/device_bridge/registration.ts
@@ -4,19 +4,19 @@ import UserManager from "../modules/user_management/model/UserManager";
 import { randomFillSync } from "crypto";
 import DeviceManager from "../modules/device_management/model/DeviceManager";
 
-/**
+/*
  * Registration module flow:
  * Stage one:
  * Client sends APP_CONTROL_OP_REGISTER with application_registration_section_t in packet. The camera ID field is empty
- * Server decodes that to a pair: MessageFromDevice::RegisterDevice and RegistrationPacket
+ * Server decodes that to a pair: OperationType::RegisterDevice and RegistrationPacket
  * If specified user ID does not exist, kill the socket
  * Server generates camera ID with at least one non-zero byte, saves it in a database, puts that into RegistrationPacket
- * Server encodes that from a pair: MessageFromDevice::RegisterDevice and RawRegistrationPacket
+ * Server encodes that from a pair: OperationType::RegisterDevice and RawRegistrationPacket
  * Server sends APP_CONTROL_OP_REGISTER with application_registration_section_t in packet to client
  * 
  * Stage two:
  * Client sends APP_CONTROL_OP_REGISTER with application_registration_section_t in packet.
- * Server decodes that to a pair: MessageFromDevice::RegisterDevice and RegistrationPacket
+ * Server decodes that to a pair: OperationType::RegisterDevice and RegistrationPacket
  * Server saves (hashed) camera auth key in database
  * Server echoes back the packet
  */
@@ -27,18 +27,19 @@ import DeviceManager from "../modules/device_management/model/DeviceManager";
  */
 // data is pass by reference, no need to return
 export async function registerDevice(data: PacketFromDevice, sock: Socket) {
-	if (await UserManager.GetUserById(data.messageContent.userID)) {
-		const device = await DeviceManager.GetDeviceByID(data.messageContent.cameraID);
+	const registrationData = data.messageContent as RawRegistrationPacket;
+	if (await UserManager.GetUserById(registrationData.userID)) {
+		const device = await DeviceManager.GetDeviceByID(registrationData.cameraID);
 		if (device) {
 			if (device.get("registration_first_stage") == true) {
 				// Registration stage two
-				device.set("auth_key", Buffer.from(data.messageContent.cameraAuthKey));
+				device.set("auth_key", Buffer.from(registrationData.cameraAuthKey));
 				device.set("registration_first_stage", false);
 				await device.save();
 				data.controlSegment.isResponse = true;
 				sock.write(data.serialize());
 			} else {
-				// Device sent register packet despite having been registered already
+				console.log("Device is trying to register itself again");
 				sock.end();
 			}
 		} else {
@@ -47,13 +48,14 @@ export async function registerDevice(data: PacketFromDevice, sock: Socket) {
 			do {
 				randomFillSync(generatedDeviceID);
 			} while (await DeviceManager.GetDeviceByID(generatedDeviceID));
-			if (typeof await DeviceManager.CreateDevice(generatedDeviceID, data.messageContent.deviceMacAddress, data.messageContent.cameraAuthKey, data.messageContent.userID) == "string") {
-				data.messageContent.cameraID = generatedDeviceID;
+			if (typeof await DeviceManager.CreateDevice(generatedDeviceID, registrationData.deviceMacAddress, registrationData.cameraAuthKey, registrationData.userID) != "string") {
+				registrationData.cameraID = generatedDeviceID;
 				data.controlSegment.isResponse = true;
 				sock.write(data.serialize());
 			}
 		}
 	} else {
+		console.log("Device sent nonexistent user ID to register");
 		sock.end();
 	}
 }
