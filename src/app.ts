e2a0f2460e7ff783d3fe9863751c19aed7767f02
adamski234 <adamski234@protonmail.com>
2024-06-05 03:57:17 +0200

    Prepare device bridge for integration

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,16 @@
 import express from 'express';
 import userRoutes from './modules/user_management/routes/userRoutes';
 import deviceRoutes from './modules/device_management/routes/deviceRoutes';
+import { DeviceConnectingServer } from "./device_bridge/app";
 
 const app = express();
 app.use(express.json());
 app.use(userRoutes.router);
 app.use(deviceRoutes.router);
 
+// Set up the device server to be globally available
+const deviceServer = new DeviceConnectingServer();
+deviceServer.start(3333);
+app.locals["deviceConnector"] = deviceServer;
 
 export default app; // Export the app object as the default export
