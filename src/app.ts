eaa069fbfdef26b896e6201d26f39877402e29bd
Pavel Radkevich <240365@edu.p.lodz.pl>
2024-06-06 00:42:01 +0200

    Skeleton bridge server - application ready. [NOT TESTED] Not tested photo transfer via WebSocke

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,25 @@ import express from 'express';
 import userRoutes from './modules/user_management/routes/userRoutes';
 import deviceRoutes from './modules/device_management/routes/deviceRoutes';
 import { DeviceConnectingServer } from "./device_bridge/app";
+import { WebSocketExpress, Router } from 'websocket-express';
+import { randomFillSync } from 'crypto';
 
-const app = express();
+const app = new WebSocketExpress();
+
 app.use(express.json());
 app.use(userRoutes.router);
 app.use(deviceRoutes.router);
+const server = app.createServer();
+server.listen(8091);
+
+let userIDBuffer = new Uint8Array(16);
+randomFillSync(userIDBuffer);
+var a = Buffer.from(userIDBuffer).toString('utf8');
+console.log(Buffer.from(userIDBuffer));
+console.log(a);
+var b = Buffer.from(a, 'utf8');
+console.log(b);
+console.log(b.toString('utf8'));
 
 // Set up the device server to be globally available
 const deviceServer = new DeviceConnectingServer();
