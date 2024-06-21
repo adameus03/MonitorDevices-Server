687e1f6582b4d208e1492cdd5d66190496455e47
mundus <mundus@pc6.home>
2024-06-21 11:53:43 +0200

    Dockerized typescript app and the scaffold analysis module

--- helpers/config.sh
+++ helpers/config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Set the location where persistent volumes, such as database files, will be stored
-export PERSISTENT_VOL_LOCATION="$HOME/monitordevices"
-export SQLITE_DBS_LOCATION="$HOME/monitordevices/sqlite/databases"
+export PERSISTENT_VOL_LOCATION="/sau"
+export SQLITE_DBS_LOCATION="/sau/sqlite/databases"
