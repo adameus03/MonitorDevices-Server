96f2d3e487e1ee27a6917839380f29ffd133e4ac
mundus <mundus@pc6.home>
2024-06-22 17:09:09 +0200

    Ensure fake user exists. Still getting 'Suspisciously large' bug, wrr

--- src/fake.sh
+++ src/fake.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 SCRIPT_NICKNAME="Faker"
+
+#Connect to the database and check if user_id 0x0123456789abcdef (16B blob) exists
+# If not create such user
+# (It is required for the fake device to work)
+
+if [[ $(sqlite3 $SQLITE_DBS_LOCATION/monitordevicesdb.sqlite "SELECT * FROM users WHERE user_id = X'000102030405060708090a0b0c0d0e0f'") ]]; then
+    echo "$SCRIPT_NICKNAME: Fake user exists"
+else
+    echo "$SCRIPT_NICKNAME: Fake user does not exist, creating..."
+    
+    sqlite3 $SQLITE_DBS_LOCATION/monitordevicesdb.sqlite "INSERT INTO users (user_id, username, password, email, createdAt, updatedAt) VALUES(X'000102030405060708090a0b0c0d0e0f', 'test_user', 'test_password', 'test_user@testmail.com', '2007-01-01 10:00:00', '2007-01-01 10:00:00')";
+
+    if [[ $(sqlite3 $SQLITE_DBS_LOCATION/monitordevicesdb.sqlite "SELECT * FROM users WHERE user_id = X'000102030405060708090a0b0c0d0e0f'") ]]; then
+        echo "$SCRIPT_NICKNAME: Fake user created successfully"
+    else
+        echo "$SCRIPT_NICKNAME: ERROR - Failed to create fake user"
+        exit 1
+    fi
+fi
+
+# Sleep to drastically increase the chance of node server starting before the fake device
 sleep 5
 # Start fake device
 cd ./fake/fakedev
