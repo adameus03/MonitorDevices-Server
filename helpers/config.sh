e710903c509d43657bceb6be0dfbce340c0e79bd
mundus <mundus@pc6.home>
2024-07-04 11:23:13 +0200

    Removed cached config.sh

--- helpers/config.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Set the location where persistent volumes, such as database files, will be stored
-export PERSISTENT_VOL_LOCATION="/sau"
-export SQLITE_DBS_LOCATION="/sau/sqlite/databases"
